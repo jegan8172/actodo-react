49a1d6fe9c7d61aba9fcb670701d4029dd18d822
jegan8172 <jeganpriyan8172@gmail.com>
2025-03-15 10:46:30 +0530

    update app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 function Header(props){
 return(<div>
-     <h1 className="text-3xl font-medium">Hello {props.username}</h1>
+     <h1 className="text-3xl font-medium">Hello {props.name}</h1>
      <p>I help you manage your activities :)</p>
 </div>
    
